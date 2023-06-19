86a71ba75abf60b82c6f37c0bb617247e424f00a
alan-junqueira <contato.alanjunqueira@gmail.com>
2023-06-19 16:43:19 -0300

    feat: start to add properties to entities and use cases.

--- src/domain/entities/student.ts
+++ src/domain/entities/student.ts
@@ -1,3 +1,11 @@
-class Student {
-  
+import { randomUUID } from "node:crypto";
+
+export class Student {
+  public name: string
+  public id: string
+
+  constructor(name: string, id?: string) {
+    this.name = name;
+    this.id = id ?? randomUUID()
+  }
 }
