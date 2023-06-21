33469316a30897e54c6283b7617ed4e58c2151df
alan-junqueira <contato.alanjunqueira@gmail.com>
2023-06-21 16:46:24 -0300

    feat: add getters and setters on answer and question entity.

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -2,6 +2,7 @@ import { Slug } from "./value-objects/slug";
 import { Entity } from "../../core/entity/entity";
 import { UniqueEntityId } from "../../core/entity/unique-entity-id";
 import { Optional } from "../../core/types/optional";
+import dayjs from "dayjs";
 
 interface IQuestionProps {
   authorId: UniqueEntityId
@@ -14,9 +15,69 @@ interface IQuestionProps {
 }
 
 export class Question extends Entity<IQuestionProps> {
-  static create(props: Optional<IQuestionProps, 'createdAt'>, id?: UniqueEntityId) {
+  get authorId() {
+    return this.props.authorId
+  }
+
+  get bestAnswerId() {
+    return this.props.bestAnswerId
+  }
+
+  get content() {
+    return this.props.content
+  }
+
+  get slug() {
+    return this.props.slug
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get title() {
+    return this.props.title
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  get isNew(): boolean {
+    return dayjs().diff(this.createdAt, 'days') <= 3
+  }
+
+  get excerpt() {
+    return this.content.substring(0, 120).trimEnd().concat('...')
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  set title(title: string) {
+    this.props.title = title
+    this.props.slug = Slug.createFromText(title)
+
+    this.touch()
+  }
+
+  set content(content: string) {
+    this.props.content = content
+
+    this.touch()
+  }
+
+  set bestAnswerId(bestAnswerId: UniqueEntityId | undefined) {
+    this.props.bestAnswerId = bestAnswerId
+
+    this.touch()
+  }
+
+  static create(props: Optional<IQuestionProps, 'createdAt' | 'slug'>, id?: UniqueEntityId) {
     const question = new Question({
       ...props,
+      slug: props.slug ?? Slug.createFromText(props.title),
       createdAt: new Date()
     }, id)
 
