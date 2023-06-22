dca668d7c853a66c0e76608b4f0033bf75888109
alan-junqueira <contato.alanjunqueira@gmail.com>
2023-06-22 18:47:47 -0300

    feat: add fakerjs, to generate fake datas on tests

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -1,17 +1,20 @@
+import { faker } from '@faker-js/faker'
+
 import { UniqueEntityId } from "@/core/entity/unique-entity-id";
 import { IQuestionProps, Question } from "@/domain/forum/enterprise/entities/question";
 import { Slug } from "@/domain/forum/enterprise/entities/value-objects/slug";
 
 export function makeQuestion(
-  override: Partial<IQuestionProps> = {}
+  override: Partial<IQuestionProps> = {},
+  id?: UniqueEntityId,
 ) {
   const question = Question.create({
     authorId: new UniqueEntityId(),
-    title: "Example question",
+    title: faker.lorem.sentence(),
     slug: Slug.create('example-question'),
-    content: 'Example content',
+    content: faker.lorem.text(),
     ...override
-  })
+  }, id)
 
   return question
 }
